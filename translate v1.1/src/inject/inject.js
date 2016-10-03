de3b80dc7b4dac4335264f8ab72acf3a53135647
shpp-vhrebenko <vhrebenko@gmail.com>
2016-10-03 09:21:24 +0300

    edit: inject.js - add popup window for select text

--- translate v1.1/src/inject/inject.js
+++ translate v1.1/src/inject/inject.js
@@ -21,32 +21,33 @@ function doKeyPress(e){
 }
 
 function createPopupWindow(event) {
-	positionX = event.clientX;
-	positionY = event.clientY;
+	positionX = event.pageX;
+	positionY = event.pageY;
 }
  
 chrome.runtime.onMessage.addListener(
  function(request, sender) {
- 	/*var newDiv = document.createElement('div')
+ 	var newDiv = document.createElement('div')
  	newDiv.id = "newDiv";
-	newDiv.style.cssText = 'position:absolute;width:200px;height:200px;-moz-border-radius:100px;border:1px  solid #ddd;-moz-box-shadow: 0px 0px 8px  #fff;display:none;';
- 	newDiv.style.top = positionY;
- 	newDiv.style.left = positionX;
-	newDiv.innerHTML = "транскрипция -- " + request.word + "\n\r" + 
-  		"слово -- " + request.reading + "\n\r" + 
-  		"перевод -- " + request.english_definitions + "\n\r" + 
-  		"часть речи -- " + request.part_of_speech ;
+	newDiv.style.cssText = 'position:absolute;width:200px;height:200px;-moz-border-radius:100px;border:1px  solid #ddd;-moz-box-shadow: 0px 0px 8px  #fff;display: block;background-color: white;';
+ 	newDiv.style.top = positionY + "px";
+ 	newDiv.style.left = positionX + "px";
 
-  	var body = document.querySelector("body");
-  	body.style.position = "relative";	
-  	console.log(body);
+	newDiv.innerHTML = "<p>транскрипция -- " + request.word + "</br>" + 
+  		"слово -- " + request.reading + "</br>" + 
+  		"перевод -- " + request.english_definitions + "</br>" + 
+  		"часть речи -- " + request.part_of_speech + "</p>";
 
-  	body.appendChild(newDiv);*/
+	var body = document.querySelector("body");
+	body.style.position = "relative";	
+	console.log(body);
 
-  alert("транскрипция -- " + request.word + "\n\r" + 
+	body.appendChild(newDiv);
+
+  /*alert("транскрипция -- " + request.word + "\n\r" + 
   		"слово -- " + request.reading + "\n\r" + 
   		"перевод -- " + request.english_definitions + "\n\r" + 
   		"часть речи -- " + request.part_of_speech );  
-  }
+  }*/
 
 );
