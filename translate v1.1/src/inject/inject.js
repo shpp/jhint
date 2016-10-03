69e76bf271cc7ebae659bf8a951b45bde798539c
shpp-vhrebenko <vhrebenko@gmail.com>
2016-10-03 18:04:17 +0300

    edit: inject.js edit popup window

--- translate v1.1/src/inject/inject.js
+++ translate v1.1/src/inject/inject.js
@@ -9,8 +9,7 @@ if (window == top) {
  
 function doKeyPress(e){
  if (e.keyCode == trigger_key){
- 	var selObj = window.getSelection();
- 	console.log(selObj);
+ 	//var selObj = window.getSelection(); 	
  	var sel = window.getSelection().toString();
  	if(sel.length) {
  		//alert("Contentscript отправил сообщение background script: '" + sel  + "'");
@@ -29,25 +28,21 @@ chrome.runtime.onMessage.addListener(
  function(request, sender) {
  	var newDiv = document.createElement('div')
  	newDiv.id = "newDiv";
-	newDiv.style.cssText = 'position:absolute;width:200px;height:200px;-moz-border-radius:100px;border:1px  solid #ddd;-moz-box-shadow: 0px 0px 8px  #fff;display: block;background-color: white;';
- 	newDiv.style.top = positionY + "px";
+	newDiv.style.cssText = 'position:absolute;padding: 1rem;border-radius:20px;border:1px solid #ddd;box-shadow: 0px 0px 8px  #fff;display: block;background-color: white;'; 	
+
+	newDiv.innerHTML = "<p>транскрипция -- " + request.word + "</p>" + 
+  		"<p>слово -- " + request.reading + "</p>" + 
+  		"<p>перевод -- " + request.english_definitions + "</p>" + 
+  		"<p>часть речи -- " + request.part_of_speech + "</p>";  
+	var body = document.querySelector("body");
+	body.style.position = "relative";
+	body.appendChild(newDiv);
+	var width = document.getElementById("newDiv").offsetWidth;  
+	var height = document.getElementById("newDiv").offsetHeight;  	
+	var currentY = positionY - (height + 100);
+  	newDiv.style.top = currentY + "px";
  	newDiv.style.left = positionX + "px";
 
-	newDiv.innerHTML = "<p>транскрипция -- " + request.word + "</br>" + 
-  		"слово -- " + request.reading + "</br>" + 
-  		"перевод -- " + request.english_definitions + "</br>" + 
-  		"часть речи -- " + request.part_of_speech + "</p>";
+	
 
-	var body = document.querySelector("body");
-	body.style.position = "relative";	
-	console.log(body);
-
-	body.appendChild(newDiv);
-
-  /*alert("транскрипция -- " + request.word + "\n\r" + 
-  		"слово -- " + request.reading + "\n\r" + 
-  		"перевод -- " + request.english_definitions + "\n\r" + 
-  		"часть речи -- " + request.part_of_speech );  
-  }*/
-
-);
+});
