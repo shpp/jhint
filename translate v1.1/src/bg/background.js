16de8f392cab28bbcf3944e209f146cd8b751462
nsmirnova <smirnovanastya1602@gmail.com>
2016-10-13 19:17:23 +0300

    Code review

--- translate v1.1/src/bg/background.js
+++ translate v1.1/src/bg/background.js
@@ -1,39 +1,35 @@
-chrome.extension.onRequest.addListener(function(request, sender)
-{
-    alert("Background got message from contentscript:'" + request.message + "'");
+chrome.extension.onRequest.addListener(function(request, sender) {
+  var words = request.message;
+  var jax = new XMLHttpRequest();
 
-    var words = request.message;
-    var jax = new XMLHttpRequest();
-    jax.open("GET","http://jisho.org/api/v1/search/words?keyword=" + words);   
-    jax.send();
-    jax.onreadystatechange = function() { 
-        if(jax.readyState==4) {
-          var responseText = jax.responseText; 
-          console.log("result" + responseText); 
-          var responseObject = JSON.parse(responseText);
-          var data = responseObject.data;
-          console.log(data);
-          var current = data[0];
-          returnMessage(current);
-        }
+  jax.open("GET","http://jisho.org/api/v1/search/words?keyword=" + words);
+  jax.send();
+  jax.onreadystatechange = function() {
+    if (jax.readyState == 4) {
+      var responseText = jax.responseText;
+      var responseObject = JSON.parse(responseText);
+      var data = responseObject.data;
+      var current = data[0];
+
+      returnMessage(current);
     }
-
- 
+  };
 });
- 
-function returnMessage(current)
-{
-    var japanese = current.japanese[0];
-    var tags = current.tags[0];
-    var senses = current.senses[0];
-    var english_definitions = senses.english_definitions;
-    var part_of_speech = senses.part_of_speech;
-    chrome.tabs.getSelected(null, function(tab) {      
-        alert("Background script send message to contentscript:'");
-        chrome.tabs.sendMessage(tab.id, {word: japanese.word,
-                                         reading: japanese.reading,
-                                         tags: tags,
-                                         english_definitions: english_definitions, 
-                                         part_of_speech: part_of_speech});
+
+function returnMessage(current) {
+  var japanese = current.japanese[0];
+  var tags = current.tags[0];
+  var senses = current.senses[0];
+  var english_definitions = senses.english_definitions;
+  var part_of_speech = senses.part_of_speech;
+
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.sendMessage(tab.id, {
+      word: japanese.word,
+      reading: japanese.reading,
+      tags: tags,
+      english_definitions: english_definitions,
+      part_of_speech: part_of_speech
     });
+  });
 }
--- translate v1.1/src/inject/inject.js
+++ translate v1.1/src/inject/inject.js
@@ -1,75 +1,64 @@
+var trigger_key = 74; //ASCII key code for the letter 'J'
+var positionX = 0;
+var positionY = 0;
 
-    var trigger_key = 74; //ASCII key code for the letter 'J'
-	var positionX = 0;
-	var positionY = 0;
-	 
-	if (window == top) {
-	    window.addEventListener('keyup', doKeyPress, false);
-	    window.addEventListener('mouseup',createPopupWindow, false);
+if (window == top) {
+    window.addEventListener('keyup', doKeyPress, false);
+    window.addEventListener('mouseup',createPopupWindow, false);
+}
+
+function doKeyPress(e) {
+	if (e.keyCode == trigger_key) {
+	 	var sel = window.getSelection().toString();
+
+	 	if (sel.length) {
+	 		chrome.extension.sendRequest({message: sel});
+	 	}
+
 	}
-	 
-	function doKeyPress(e){
-		if (e.keyCode == trigger_key){ 		
-		 	var sel = window.getSelection().toString();
-		 	if(sel.length) {
-		 		alert("Contentscript send message background script: '" + sel  + "'");
-		 		chrome.extension.sendRequest({message: sel});
-		 	}  
-		  
-		}
-	}
+}
 
-	function createPopupWindow(event) {
-		positionX = event.pageX;
-		positionY = event.pageY;
-	}
-	 
-	chrome.runtime.onMessage.addListener(
-		function(request, sender) {
-			alert("Contentscript send get message from background script");
-		 	var $newDiv = $('<div>');
-		 	var $contentDiv = $('<div>');
-		 	$contentDiv.attr("id", "contentDiv");
-		 	$newDiv.attr("id", "newDiv");
+function createPopupWindow(event) {
+	positionX = event.pageX;
+	positionY = event.pageY;
+}
 
-		 	if(request.english_definitions != undefined) {
-		 		var arr = request.english_definitions;		 		
-				var str = "";
-				for (var i = 0; i < arr.length; i++) {
-					str = str + arr[i] + ", "
-				};
-				request.english_definitions = str;
-			} else {
-				request.english_definitions = "";
-			}	
-			if(request.reading == undefined) {
-				request.reading = "";
-			} else if (request.word == undefined) {
-				request.word = "";
-			} else if(request.part_of_speech == undefined) {
-				request.part_of_speech = "noun";
-			}
-			$contentDiv.append($('<a>').addClass("boxclose").attr("id","boxclose"))
-				  .append($('<p>').text(request.reading))	
-				  .append($('<p>').text(request.word).addClass("word")) 
-				  .append($('<p>').text(request.english_definitions))
-		  		  .append($('<p>').text(request.part_of_speech))
-		  		  .append($('<p>')
-		  		  	.append($('<a>').attr("href","http://jisho.org/").text("jlpt n5")));	  		 
-		  	$newDiv.append($contentDiv);	 
-			var $body = $('body');				
-			$body.append($newDiv);
-			$boxclose = $("#boxclose"); 
-			var width = $("#newDiv").innerWidth();  
-			var height = $("#newDiv").innerHeight();  	
-			var currentY = positionY + 25;
-			var currentX = positionX - 25;
-		  	$newDiv.css("top", currentY + "px");
-		 	$newDiv.css("left", currentX + "px");
+chrome.runtime.onMessage.addListener(function(request, sender) {
+ 	var $newDiv = $('<div>');
+ 	var $contentDiv = $('<div>');
 
-		 	$boxclose.click(function(event){
-				$newDiv.remove();
-			})
+ 	$contentDiv.attr("id", "jhint_contentDiv");
+ 	$newDiv.attr("id", "jhint_newDiv");
 
+  request.english_definitions = request.english_definitions ? request.english_definitions.join(', ') : '';
+  request.reading = request.reading || '';
+  request.word = request.word || '';
+  request.part_of_speech = request.part_of_speech || 'noun';
+
+  var $character = $('<ruby>').text(request.word).append($('<rt>').text(request.reading));
+
+	$contentDiv
+    .append($('<a>').addClass("jhint_boxclose").attr("id", "jhint_boxclose"))
+    .append($('<p>').append($character))
+	  .append($('<p>').text(request.english_definitions))
+  	.append($('<p>').append($('<span class="jhint_partOfSpeech">').text(request.part_of_speech)))
+  	.append($('<p>').append($('<a>').attr("href","http://jisho.org/").text('jisho.org')));
+
+  $newDiv.append($contentDiv);
+
+	$('body').append($newDiv);
+
+	$boxclose = $("#jhint_boxclose");
+
+	var width = $("#jhint_newDiv").innerWidth();
+	var height = $("#jhint_newDiv").innerHeight();
+	var currentY = positionY + 25;
+	var currentX = positionX - 25;
+
+	$newDiv.css("top", currentY + "px");
+ 	$newDiv.css("left", currentX + "px");
+
+ 	$boxclose.click(function(event) {
+		$newDiv.remove();
 	});
-
+});
